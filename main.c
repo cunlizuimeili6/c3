494e5a0d821764f577411d05621a3987fdffd491
Your Name <zhangxueying@qq.com>
2019-01-17 09:19:29 +0800

    输入和调试

--- main.c
+++ main.c
@@ -29,12 +29,56 @@ int main(){
         if(code==2){
             char x;
             if(index>0){
+                index--;
                 printf("chenggongjixu\n");
+                scanf("%c",&x);
+                scanf("%c",&x);
             }else{
                 printf("shibaijixu\n");
                 scanf("%c",&x);
                 scanf("%c",&x);
             }
+        }
+                if(code==3){
+            char x;
+            if(index>0){
+                for(int i=0;index>i;i++){
+                printf("di %d yi %c\n",i+1,arr[i]);    
+                }
+            }else{
+                printf("wu\n");
+            }
+            printf("jixu\n");
+            scanf("%c",&x);
+            scanf("%c",&x);
+            }
+        if(code==4){
+            char x;
+            char str[200];
+            int i=0;
+            printf("shuruyingwen\n");
+            scanf("%s",str);
+            for(;str[i]!='\0';i++){
+                char a=str[i];
+                int j=0;
+                int flag=0;
+                for(;index>j;j++){
+                    if(a==arr[j]){
+                    flag=1;
+                }
+            }
+            if(flag!=0){
+                str[i]='*';
+            }
+        }
+        printf("%s\n",str);
+        printf("jixu\n");
+        scanf("%c",&x);
+        scanf("%c",&x);
+    }
+        if(code==5){
+            printf("tuichu\n");
+            break;
         }
     }
     return 0;
